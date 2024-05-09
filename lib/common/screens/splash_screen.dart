ab48dd636ce55ba4c87b8019d89f29074f65bf2a
Rashid Yousaf <hacktonow@gmail.com>
2024-05-09 11:53:13 +0500

    merge master

--- lib/common/screens/splash_screen.dart
+++ lib/common/screens/splash_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ride_options_2/common/cubits/theme/theme_cubit.dart';
-import 'package:ride_options_2/common/cubits/theme/theme_state.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -13,21 +10,12 @@ class SplashScreen extends StatelessWidget {
       body: Center(
         child: SizedBox(
             width: 361.w,
-            height: 80.h,
-            child: BlocConsumer<ThemeCubit, ThemeState>(
-              listener: (context, state) {
-                // TODO: implement listener
-              },
-              builder: (context, state) {
-                return ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<ThemeCubit>(context).toggleTheme();
-                    },
-                    child: const Text(
-                      "Click Me",
-                    ));
-              },
-            )),
+            height: 57.h,
+            child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Click Me",
+                ))),
       ),
     );
   }
--- lib/common/theme/cubits/theme_cubit.dart
+++ lib/common/theme/cubits/theme_cubit.dart
@@ -3,13 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-
   bool isDarkMode = false;
 
   ThemeCubit() : super(ThemeState().init());
 
   void toggleTheme() {
-      isDarkMode = !isDarkMode;
-      emit(ChangeTheme());
+    isDarkMode = !isDarkMode;
+    emit(ChangeTheme());
   }
 }
